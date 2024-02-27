597cdddbb22ba63f81c5527cbcb05ea37a9ee6fd
lorenzovizcaino <lorenzovizcaino@gmail.com>
2024-02-27 22:00:59 +0100

    27/02/24_22:00

--- src/main/java/com/example/practicacincojavafx/Controller.java
+++ src/main/java/com/example/practicacincojavafx/Controller.java
@@ -1,13 +1,28 @@
 package com.example.practicacincojavafx;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class Controller {
+import java.io.InterruptedIOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ListView<String> lvFiguras = new ListView<>();
 
     @FXML
     private MenuButton SelecionColor;
@@ -35,10 +50,48 @@ public class Controller {
 
     @FXML
     private TextField tfRadio;
+    private ObservableList<String> listaFiguras;
 
     @FXML
     void selectColor(ActionEvent event) {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        tfAncho.setDisable(true);
+        tfAlto.setDisable(true);
+        tfRadio.setDisable(true);
+        tfLado.setDisable(true);
+        listaFiguras = FXCollections.observableArrayList("Cuadrado", "Triangulo", "Circulo");
+        lvFiguras.setItems(listaFiguras);
+        lvFiguras.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String figAntigua, String figNueva) {
+                if(figNueva.equals("Cuadrado")){
+                    tfAncho.setDisable(false);
+                    tfAlto.setDisable(false);
+                    tfRadio.setDisable(true);
+                    tfLado.setDisable(true);
+                }
+                if(figNueva.equals("Triangulo")){
+                    tfAncho.setDisable(true);
+                    tfAlto.setDisable(true);
+                    tfRadio.setDisable(true);
+                    tfLado.setDisable(false);
+                }
+                if(figNueva.equals("Circulo")){
+                    tfAncho.setDisable(true);
+                    tfAlto.setDisable(true);
+                    tfRadio.setDisable(false);
+                    tfLado.setDisable(true);
+                }
+            }
+        });
+    }
+
 }
+
+
+
